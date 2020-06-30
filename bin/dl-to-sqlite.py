e7cfb8b00e369b835f277bd52502dae3df83962e
Ken Pu <ken.pu@uoit.net>
2020-06-30 02:15:07 -0400

    change for the new API endpoint

--- bin/dl-to-sqlite.py
+++ bin/dl-to-sqlite.py
@@ -19,8 +19,7 @@ def log(s=""):
     sys.stderr.write(s + "\n")
     sys.stderr.flush()
 
-
-def download(semester, subjs):
+def subjs_param(subjs):
     all_subjs = [
     'ALSU',
     'AEDT',
@@ -69,29 +68,32 @@ def download(semester, subjs):
     'STAT',
     'WMST',
     ]
+    if not subjs:
+        subjs = all_subjs
+    elif isinstance(subjs, str):
+        subjs = [subjs]
 
-    if not subjs: subjs = all_subjs
-    subj = "&".join('sel_subj=%s' % x for x in subjs)
+    return "&".join("sel_subj=%s" % x for x in subjs)
 
-    # url = "https://ssbp.mycampus.ca/prod/bwckschd.p_get_crse_unsec"
-    url = "http://ssbp.mycampus.ca/prod_uoit/bwckschd.p_get_crse_unsec"
-    formdata = """
-        TRM=U&term_in=%s&sel_subj=dummy&sel_day=dummy
-        &sel_schd=dummy&sel_insm=dummy&sel_camp=dummy
-        &sel_levl=dummy&sel_sess=dummy&sel_instr=dummy
-        &sel_ptrm=dummy&sel_attr=dummy
-        &%s
-        &sel_crse=&sel_title=&sel_from_cred=
-        &sel_to_cred=&sel_camp=%%25&begin_hh=0
-        &begin_mi=0&begin_ap=a&end_hh=0&end_mi=0
-        &end_ap=a""" % (semester, subj)
+def cleanup(s):
+    return re.sub(r'\s+', "", s)
 
-    formdata = "".join([x.strip() for x in formdata.split("\n")]).strip()
+def download(semester, subjs):
+    url = cleanup("""
+    https://ssbp.mycampus.ca/prod_uoit/bwckschd.p_get_crse_unsec?TRM=U
+    &term_in={}
+    &sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy
+    &sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy
+    &{}
+    &sel_crse=&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25
+    &sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a
+    """.format(semester, subjs_param(subjs)))
+
     text = None
 
     log("Downloading from %s" % url)
     
-    r = requests.post(url, data=formdata)
+    r = requests.get(url)
     text = r.text
     with open("/tmp/mycampus.r", "w") as f:
         f.write(text)
